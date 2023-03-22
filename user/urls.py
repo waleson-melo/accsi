2611c2f4cbd6d32d612a7073cbeed8271873e2ac
Waleson Melo <walesonmelo23@gmail.com>
2023-03-22 16:41:46 -0400

    -a

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from .views import CreateUser, UpdateUser, ListUser
 app_name = 'user'
 
 urlpatterns = [
-    path('cadastrar/', CreateUser.as_view(), name='create'),
-    path('alterar/<int:pk>', UpdateUser.as_view(), name='update'),
+    path('cadastrar/<int:meu>/', CreateUser.as_view(), name='create'),
+    path('alterar/<int:pk>/', UpdateUser.as_view(), name='update'),
     path('usuarios/', ListUser.as_view(), name='list'),
 ]
--- user/views.py
+++ user/views.py
@@ -11,11 +11,19 @@ class CreateUser(CreateView):
     fields = ['email', 'username', 'first_name', 'last_name', 'password']
     success_url = reverse_lazy('user:list')
 
+    def dispatch(self, request, *args, **kwargs):
+        self.aluno = kwargs.get('meu', None)
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_title'] = 'Cadastrar Usuário'
+        context['page_title'] = 'Cadastrar' + str(self.aluno)
 
         return context
+
+    def form_valid(self, form):
+        # form.instance.email = 'haro'
+        return super().form_valid(form)
 
 
 class UpdateUser(UpdateView):
@@ -41,3 +49,8 @@ class ListUser(ListView):
         context['page_title'] = 'Usuários'
 
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(id=2)
+        return queryset
